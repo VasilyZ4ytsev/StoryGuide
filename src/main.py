2394e9cb32962605e78598415ba4658792d37689
vasya <mc_lovin_65@mail.ru>
2026-02-09 14:12:36 +0500

    Начальная подготовка проекта: структура, зависимости и документация

---
+++ src/main.py
@@ -0,0 +1,7 @@
+﻿import streamlit as st
+
+st.title("Привет, Проект!")
+st.write("Если вы видите этот текст, значит Streamlit работает отлично.")
+
+if st.button("Нажми меня"):
+    st.success("Все системы в норме!")
